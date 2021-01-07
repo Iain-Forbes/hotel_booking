158c7689eb5318a03360713ce5397881946848dc
Iain Forbes <i.forbes77@gmail.com>
2021-01-07 11:53:24 +0000

    server.js

---
+++ server/server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+const parser = require('body-parser');
+app.use(parser.json());
+const cors = require('cors');
+app.use(cors());
+
+const MongoClient = require('mongodb').MongoClient
+const createRouter =
