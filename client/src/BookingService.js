c4291762ea76f83cb517e03e07628a49849ae6ea
Iain Forbes <i.forbes77@gmail.com>
2021-01-07 13:07:44 +0000

    added post

--- client/src/BookingService.js
+++ client/src/BookingService.js
@@ -4,3 +4,13 @@ export const getBookings = () => {
    return fetch(baseURL)
    .then(result => result.json())
 }
+
+export const postBooking = (payload) => {
+    return fetch(baseURL, {
+        method: "POST",
+        body: JSON.stringify(payload),
+        headers: { 'Content-Type': 'application/json' }
+    })
+    .then(res => res.json())
+   
+}
